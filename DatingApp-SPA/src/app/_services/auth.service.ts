1470d5ca3eb87c33edefc908758f0dd502614dbe
Ly Sang <lyphuocsang96@outlook.com>
2020-03-07 01:57:56 +0700

    commit

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -10,6 +10,7 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
   jwtHelper = new JwtHelperService();
+  decodeToken: any;
 
 constructor(private http: HttpClient) { }
 
@@ -19,6 +20,7 @@ login(model: any) {
       const user = response;
       if (user) {
         localStorage.setItem('token', user.token);
+        this.decodeToken = this.jwtHelper.decodeToken(user.token);
       }
     })
   );
