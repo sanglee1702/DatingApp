0240a493f8692f110596a548bd284c0379383dcc
Sang Lee <lyphuocsang96@gmail.com>
2020-06-20 22:55:42 +0700

    9.5

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,50 +1,50 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownModule } from 'ngx-bootstrap';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BsDropdownModule } from "ngx-bootstrap";
+import { RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { AuthService } from './_services/auth.service';
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
-import { ErrorInterceptorProvider } from './_services/error.interceptor';
-import { AlertifyService } from './_services/alertify.service';
-import { ListsComponent } from './lists/lists.component';
-import { MemberListComponent } from './member-list/member-list.component';
-import { MessagesComponent } from './messages/messages.component';
-import { appRoutes } from './routes';
-import { AuthGuard } from './_guards/auth.guard';
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
+import { AuthService } from "./_services/auth.service";
+import { RegisterComponent } from "./register/register.component";
+import { HomeComponent } from "./home/home.component";
+import { ErrorInterceptorProvider } from "./_services/error.interceptor";
+import { AlertifyService } from "./_services/alertify.service";
+import { ListsComponent } from "./lists/lists.component";
+import { MemberListComponent } from "./member-list/member-list.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { appRoutes } from "./routes";
+import { AuthGuard } from "./_guards/auth.guard";
+import { UserService } from "./_services/user.service";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      NavComponent,
-      RegisterComponent,
-      HomeComponent,
-      ListsComponent,
-      MemberListComponent,
-      MessagesComponent
-   ],
-   imports: [
-      BrowserModule,
-      HttpClientModule,
-      FormsModule,
-      BrowserAnimationsModule,
-      BsDropdownModule.forRoot(),
-      RouterModule.forRoot(appRoutes)
-   ],
-   providers: [
-      AuthService,
-      ErrorInterceptorProvider,
-      AlertifyService,
-      AuthGuard
-   ],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    NavComponent,
+    RegisterComponent,
+    HomeComponent,
+    ListsComponent,
+    MemberListComponent,
+    MessagesComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+  ],
+  providers: [
+    AuthService,
+    ErrorInterceptorProvider,
+    AlertifyService,
+    AuthGuard,
+    UserService,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
