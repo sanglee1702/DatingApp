660abaefc856f61e2ca49a91b1f136321a3ef79f
Sang Lee <lyphuocsang96@gmail.com>
2020-06-23 21:16:33 +0700

    11.8

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BsDropdownModule, TabsModule } from "ngx-bootstrap";
 import { RouterModule } from "@angular/router";
 import { JwtModule } from "@auth0/angular-jwt";
 import { NgxGalleryModule } from "@kolkov/ngx-gallery";
+import { FileUploadModule } from "ng2-file-upload";
 
 import { AppComponent } from "./app.component";
 import { NavComponent } from "./nav/nav.component";
@@ -28,6 +29,7 @@ import { MemberListResolver } from "./_resolver/member-list.resolver";
 import { MemberEditComponent } from "./members/member-edit/member-edit.component";
 import { MemberEditResolver } from "./_resolver/member-edit.resolver";
 import { PreventUnsavedChanges } from "./_guards/prevent-unsaved-changes";
+import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 
 export function tokenGetter() {
   return localStorage.getItem("token");
@@ -45,6 +47,7 @@ export function tokenGetter() {
     MemberCardComponent,
     MemberDetailComponent,
     MemberEditComponent,
+    PhotoEditorComponent,
   ],
   imports: [
     BrowserModule,
@@ -55,6 +58,7 @@ export function tokenGetter() {
     TabsModule.forRoot(),
     RouterModule.forRoot(appRoutes),
     NgxGalleryModule,
+    FileUploadModule,
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
---
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Photo } from 'src/app/_model/photo';
+import { FileUploader } from 'ng2-file-upload';
+import { environment } from '../../../environments/environment';
+import { AuthService } from 'src/app/_services/auth.service';
+
+@Component({
+  selector: 'app-photo-editor',
+  templateUrl: './photo-editor.component.html',
+  styleUrls: ['./photo-editor.component.css']
+})
+export class PhotoEditorComponent implements OnInit {
+  @Input() photos: Photo[];
+  uploader: FileUploader; 
+  hasBaseDropZoneOver = false;
+  baseUrl = environment.apiUrl;
+
+  constructor (private authService: AuthService){
+  }
+
+  ngOnInit() {
+    this.initializeUploader();
+  }
+  
+  public fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/' + this.authService.decodeToken.nameid + '/photos',
+      authToken: 'Bearer ' + localStorage.getItem('token'),
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    })
+  }
+}
